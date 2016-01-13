6bb276dd30e63bc9e6abe6dd62f62bc4b736e401
Christopher Henry <chenry@mcs.anl.gov>
2016-01-13 14:32:10 -0600

    Fixing bug

--- lib/Bio/ModelSEED/ProbModelSEED/ProbModelSEEDHelper.pm
+++ lib/Bio/ModelSEED/ProbModelSEED/ProbModelSEEDHelper.pm
@@ -1883,7 +1883,7 @@ sub ImportKBaseModel {
 				my $rxns = $solution->gapfillingSolutionReactions();
 				for (my $i=0; $i < @{$rxns}; $i++) {
 					my $rxn = $rxns->[$i];
-					$rxn->reaction($rxn->_buil_reaction());
+					$rxn->reaction($rxn->_build_reaction());
 					my $rxnid = $rxn->reaction()->id();
 					my $mdlrxn;
 					my $ismdlrxn = 0;
