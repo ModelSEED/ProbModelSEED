a34caf94e67ef117e504336ddc087fe7ee462a59
Christopher Henry <chenry@mcs.anl.gov>
2016-11-11 17:15:40 -0600

    Changes to get most useful models first

--- internalScripts/BuildPATRICModels.pl
+++ internalScripts/BuildPATRICModels.pl
@@ -15,7 +15,14 @@ my $procs = $ARGV[0];
 my $index = $ARGV[1];
 
 #my $modellist = Bio::KBase::ObjectAPI::utilities::LOADFILE("/Users/chenry/workspace/PATRIC_genomes/GenomeList.txt");
-my $modellist = Bio::KBase::ObjectAPI::utilities::LOADFILE("/homes/chenry/PATRICGenomeList.txt");
+#my $modellist = Bio::KBase::ObjectAPI::utilities::LOADFILE("/homes/chenry/PATRICGenomeList.txt");
+my $modellist = Bio::KBase::ObjectAPI::utilities::LOADFILE("/homes/chenry/ShortGenomeList.txt");
+my $donelist = Bio::KBase::ObjectAPI::utilities::LOADFILE("/disks/p3dev2/fba/genomesuccess.out");
+my $donehash = {};
+for (my $i=0; $i < @{$donelist}; $i++) {
+	$donelist->[$i] =~ s/:success//;
+	$donehash->{$donelist->[$i]} = 1;
+}
 
 my $helper;
 Bio::KBase::ObjectAPI::config::adminmode(1);
@@ -30,34 +37,36 @@ $helper = Bio::ModelSEED::ProbModelSEED::ProbModelSEEDHelper->new({
 my $count = 0;
 for (my $i=0; $i < @{$modellist}; $i++) {
 	if ($i % $procs  == $index) {
-		if ($count % 100 == 0) {
-			$helper = undef;
-			$helper = Bio::ModelSEED::ProbModelSEED::ProbModelSEEDHelper->new({
-				token => Bio::P3::Workspace::ScriptHelpers::token(),
-				username => "chenry",
-				method => "ModelReconstruction",
-				configfile => $configfile
-			});
-			Bio::KBase::ObjectAPI::config::adminmode(1);
+		if (!defined($donehash->{$modellist->[$i]})) {
+			if ($count % 100 == 0) {
+				$helper = undef;
+				$helper = Bio::ModelSEED::ProbModelSEED::ProbModelSEEDHelper->new({
+					token => Bio::P3::Workspace::ScriptHelpers::token(),
+					username => "chenry",
+					method => "ModelReconstruction",
+					configfile => $configfile
+				});
+				Bio::KBase::ObjectAPI::config::adminmode(1);
+			}
+			Bio::KBase::ObjectAPI::config::setowner("chenry");
+			my $genome = "PATRIC:".$modellist->[$i];
+			eval {
+				$helper->app_harness("ModelReconstruction",{
+					media => "/chenry/public/modelsupport/media/ArgonneLBMedia",
+					gapfill => 1,
+					predict_essentiality => 1,
+					genome => $genome,
+					output_file => $modellist->[$i],
+					output_path => "/chenry/public/PATRICModels/"
+				});
+			};
+			if ($@) {
+				my $error = $@;
+				Bio::KBase::ObjectAPI::logging::log($i.":".$modellist->[$i].":".$error);
+			} else {
+				Bio::KBase::ObjectAPI::logging::log($i.":".$modellist->[$i].":success");
+			}
+			$count++;
 		}
-		Bio::KBase::ObjectAPI::config::setowner("chenry");
-		my $genome = "PATRIC:".$modellist->[$i];
-		eval {
-			$helper->app_harness("ModelReconstruction",{
-				media => "/chenry/public/modelsupport/media/ArgonneLBMedia",
-				gapfill => 1,
-				predict_essentiality => 1,
-				genome => $genome,
-				output_file => $modellist->[$i],
-				output_path => "/chenry/public/PATRICModels/"
-			});
-		};
-		if ($@) {
-			my $error = $@;
-			Bio::KBase::ObjectAPI::logging::log($i.":".$modellist->[$i].":".$error);
-		} else {
-			Bio::KBase::ObjectAPI::logging::log($i.":".$modellist->[$i].":success");
-		}
-		$count++;
 	}
 }
