19beba4e8766e4a419b7dd9f5568e6d629cce57e
seaver <seaver@mcs.anl.gov>
2017-08-16 09:30:35 -0500

    Fixing bugs in parsing media object

--- lib/Bio/KBase/ObjectAPI/PATRICStore.pm
+++ lib/Bio/KBase/ObjectAPI/PATRICStore.pm
@@ -472,10 +472,10 @@ sub transform_media_from_ws {
 		source_id => $meta->[0],
 		mediacompounds => []
 	};
-	my $array = [split(/\n/,$data)];
+	my $array = [split(/\r\n?|\n/,$data)];
 	my $heading = [split(/\t/,$array->[0])];
 	my $headinghash = {};
-	for (my $i=1; $i < @{$heading}; $i++) {
+	for (my $i=0; $i < @{$heading}; $i++) {
 		$headinghash->{$heading->[$i]} = $i;
 	}
 	my $biochem;
