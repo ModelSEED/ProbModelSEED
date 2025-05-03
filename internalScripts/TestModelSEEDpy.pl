7b82874fa0c0d4870077fd7a72a9871e1eaafe0b
Christopher Henry <chenry@anl.gov>
2025-05-02 23:30:25 -0500

    Debugging

--- internalScripts/TestModelSEEDpy.pl
+++ internalScripts/TestModelSEEDpy.pl
@@ -191,10 +191,11 @@ my $input = {
 };
 
 my $json = $datachannel->{fbamodel}->serializeToDB();
-print("File1:".Bio::KBase::utilities::conf("ProbModelSEED","work_dir")+"/output_model.json");
-print("File2:".Bio::KBase::utilities::conf("ProbModelSEED","work_dir")+"/input.json");
-print("Bin1:".Bio::KBase::utilities::conf("ProbModelSEED","bin_directory"));
-print("Input:",Bio::KBase::ObjectAPI::utilities::TOJSON($input));
+#print("File1:".Bio::KBase::utilities::conf("ProbModelSEED","work_dir")+"/output_model.json");
+#print("File2:".Bio::KBase::utilities::conf("ProbModelSEED","work_dir")+"/input.json");
+#print("Bin1:".Bio::KBase::utilities::conf("ProbModelSEED","bin_directory"));
+print("Config:",Bio::KBase::ObjectAPI::utilities::TOJSON(Bio::KBase::utilities::$config));
+#print("Input:",Bio::KBase::ObjectAPI::utilities::TOJSON($input));
 Bio::KBase::ObjectAPI::utilities::PRINTFILE(Bio::KBase::utilities::conf("ProbModelSEED","work_dir")+"/output_model.json",[$json]);
 Bio::KBase::ObjectAPI::utilities::PRINTFILE(Bio::KBase::utilities::conf("ProbModelSEED","work_dir")+"/input.json",[Bio::KBase::ObjectAPI::utilities::TOJSON($input)]);
 my $cmd = Bio::KBase::utilities::conf("ProbModelSEED","bin_directory")."/ModelSEEDpy-ModelReconstruction.pl ".Bio::KBase::utilities::conf("ProbModelSEED","work_dir")."/input.json";
